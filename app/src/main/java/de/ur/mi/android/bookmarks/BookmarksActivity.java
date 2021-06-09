7179a556bd05309fc0ee751da9a448a6c165ef61
luca <luca.helgath@web.de>
2021-06-09 10:51:45 +0200

    viewHolder

--- app/src/main/java/de/ur/mi/android/bookmarks/BookmarksActivity.java
+++ app/src/main/java/de/ur/mi/android/bookmarks/BookmarksActivity.java
@@ -1,15 +1,55 @@
 package de.ur.mi.android.bookmarks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+
+import de.ur.mi.android.bookmarks.bookmarks.Bookmark;
+
 public class BookmarksActivity extends AppCompatActivity {
+
+    private ArrayList<Bookmark> bookmarks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmarks);
+        bookmarks = new ArrayList<>();
+        handleIntent(getIntent());
+
+    }
+
+    private void handleIntent(Intent intent) {
+        String intentAction = intent.getAction();
+        String intentType = intent.getType();
+        if (intentAction == null || intentType == null) {
+            return;
+        }
+        if (intentAction.equals(intent.ACTION_SEND)) {
+            if (intentType.equals("text/plain")) {
+                bookmarks.add(createBookmark(intent));
+            }
+        }
+    }
+
+    private Bookmark createBookmark(Intent intent) {
+        String url = intent.getStringExtra(Intent.EXTRA_TEXT);
+        String title = intent.getStringExtra(Intent.EXTRA_SUBJECT);
+        try {
+            URL bookmarkURL = new URL(url);
+            Date creationDate = new Date();
+            return (new Bookmark(bookmarkURL, title, creationDate));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
---
+++ app/src/main/java/de/ur/mi/android/bookmarks/ui/BookmarkViewHolder.java
@@ -0,0 +1,12 @@
+package de.ur.mi.android.bookmarks.ui;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BookmarkViewHolder extends RecyclerView.ViewHolder {
+    public BookmarkViewHolder(View v) {
+        super(v);
+    }
+}
